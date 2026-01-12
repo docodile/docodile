e8ad4f84b3807dd9d18f72f587367ab71621db55
jakeec <jake.ethan.carrington@gmail.com>
2026-01-12 00:15:29 +0000

    fix snapshot tests

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -39,25 +39,25 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
+    // {
+    //   name: "Mobile Chrome",
+    //   use: { ...devices["Pixel 5"] },
+    // },
+    // {
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
+    // },
 
     /* Test against branded browsers. */
     // {
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "cd .. && make watch",
-    url: "http://localhost:6006",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer:  {
+  //   command: "cd .. && make watch",
+  //   url: "http://localhost:6006",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
--- ui-tests/tests/docodile.spec.ts
+++ ui-tests/tests/docodile.spec.ts
@@ -2,16 +2,18 @@ import { test, expect } from "@playwright/test";
 
 const pages = [
   "",
-  "about",
-  "blank",
-  "contact",
-  "colophon",
-  "defaults",
-  "follow",
-  "links",
-  "slashes",
-  "blog",
-  "blog/20260106",
+  "getting-started",
+  "getting-started/contributing",
+  "setup",
+  "setup/appearance",
+  "setup/navigation",
+  "reference",
+  "reference/markdown",
+  "reference/markdown-extensions",
+  "reference/markdown-extensions/admonitions",
+  "reference/markdown-extensions/collapsible-headings",
+  "reference/markdown-extensions/tables",
+  "reference/organisation",
 ];
 
 pages.forEach((p) => {
